ac5ba244773baadc84b830ae8f461d3f8c1e8baf
Pavel <nightcharm@mail.ru>
2023-04-27 19:04:04 +0300

    add list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,11 @@ import AppFilter from "../app-fillter/app-filter";
 import EmploeesList from "../emploees-list/emploees-list";
 import EmployeesAddForm from "../emploees-add-form/emploees-add-form";
 function App() {
+    const data = [
+        { name: "Alex", salary: 800, increase: true },
+        { name: "John", salary: 5000, increase: true },
+        { name: "Carl", salary: 3000, increase: true }
+    ]
     return (
         <div className="app">
             <AppInfo />
@@ -14,7 +19,7 @@ function App() {
                 <AppFilter />
 
             </div>
-            <EmploeesList />
+            <EmploeesList data={data} />
             <EmployeesAddForm />
         </div>
     )
--- src/components/emploees-list/emploees-list.js
+++ src/components/emploees-list/emploees-list.js
@@ -1,11 +1,14 @@
 import "./emploees-list.css"
 import EmploeesListItem from "../emploees-list-item/emploees-lits-item";
-const EmploeesList = () => {
+const EmploeesList = ({ data }) => {
+    console.log(data)
     return (
         <ul className="app-list list-group">
-            <EmploeesListItem />
-            <EmploeesListItem />
-            <EmploeesListItem />
+            {data.map(item => {
+                return <EmploeesListItem name={item.name} salary={item.salary} key={item.name + "1"} increase={item.increase} />
+
+            })}
+
         </ul>
     );
 }
