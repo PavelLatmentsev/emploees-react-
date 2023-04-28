19b6bc8b151dfcd7c2a963981d02349a16a05a7b
Pavel <nightcharm@mail.ru>
2023-04-28 10:09:06 +0300

    add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,8 @@ class App extends Component {
                 { name: "John", salary: 5000, increase: true, id: 2, like: false },
                 { name: "Carl", salary: 3000, increase: true, id: 3, like: false }
             ],
-            term: ""
+            term: "",
+            filter: "all"
         }
     }
     deleteItem = (id) => {
@@ -83,18 +84,35 @@ class App extends Component {
     onUpdateSearch = (term) => {
         this.setState(({ term }))
     }
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case "like":
+                return items.filter(item => item.like);
+
+
+            case "moreThen1000":
+                return items.filter(item => item.salary > 1000);
+
+            default:
+                return items;
+        }
+    }
+    onFilterSelect = (filter) => {
+        this.setState({ filter });
+    }
     render() {
-        const { data, term } = this.state;
+        const { data, term, filter } = this.state;
         const emploees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
         return (
             <div className="app">
                 <AppInfo emploees={emploees} increased={increased} />
 
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-                    <AppFilter />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
 
                 </div>
                 <EmploeesList
